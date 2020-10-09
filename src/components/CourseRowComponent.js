c7fc386f5c671ba573e7e09b08788fdd19d458f8
ericli0303 <33180705+ericli0303@users.noreply.github.com>
2020-10-09 13:23:09 -0400

    added courserow

---
+++ src/components/CourseRowComponent.js
@@ -0,0 +1,80 @@
+import React from "react";
+import courseService from "../services/CourseService";
+import CourseListComponent from "./CourseListComponent";
+
+
+class CourseRowComponent extends React.Component {
+    state = {
+        editing: false,
+        course: this.props.course
+    }
+    constructor(props) {
+        super(props)
+    }
+
+    updateTitle =(event) => {
+        const newTitle = event.target.value
+        const course = {
+            ...this.state.course
+        }
+        course.title = newTitle
+        this.setState({
+            course: course
+        })
+    }
+
+    updateCourse = () => {
+        this.setState({ editing: false })
+        courseService.updateCourse(this.state.course._id, this.state.course)
+    }
+    render() {
+        return (
+            <tr>
+                <td className="priority-1">
+                    {
+                        !this.state.editing &&
+                        <a href="../courseEditor/courseEditor.template.client.html">
+                            <i className="fas fa-file"></i>{this.state.course.title}
+                        </a>
+                    }
+                    {
+                        this.state.editing &&
+                        <input
+                            onChange={this.updateTitle}
+                            value={this.state.course.title} />
+                    }
+                </td>
+                <td className="priority-3">{this.props.course.owner}</td>
+                <td className="priority-2">{this.props.course.lastUpdated}</td>
+                <td className="priority-2"></td>
+                {
+                    this.state.editing &&
+                    <td className="priority-2">
+                        <button onClick={this.updateCourse}>
+                            Save
+                    </button>
+                    </td>
+                }
+                {
+                    !this.state.editing &&
+                    <td className="priority-2">
+                        <button onClick={() => this.setState({ editing: true })}>
+                            Edit
+                    </button>
+                    </td>
+                }
+                {
+                    !this.state.editing &&
+                    <td className="priority-1">
+                        <button onClick={() => this.props.deleteCourse(this.props.course)}>
+                            <i className="fa fa-times" aria-hidden="true"></i>
+                        </button>
+                    </td>
+                }
+            </tr>
+        )
+    }
+}
+
+
+export default CourseRowComponent
